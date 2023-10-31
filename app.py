ce7902ec05f1cb3aa500f85d216755c7f471b9ca
Isha1031 <iishasharrma@gmail.com>
2023-11-01 02:12:05 +0530

    Total number of messages, words and medias added

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
-import preprocessor
-
+import preprocessor, helper
+import matplotlib.pyplot as plt
 
 st.sidebar.title("Whatsapp chat Analyser")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue() #this file is currently a stream of byte data so now we will conver that file to string format
-    data = bytes_data.decode("utf-8")#decoding and converting the file inti string format
+    bytes_data = uploaded_file.getvalue()  #this file is currently a stream of byte data so now we will conver that file to string format
+    data = bytes_data.decode("utf-8") #decoding and converting the file inti string format
     df = preprocessor.preprocess(data)
 
     st.dataframe(df)
@@ -16,7 +16,34 @@ if uploaded_file is not None:
     user_list = df['sender'].unique().tolist()
     user_list.sort()
     user_list.insert(0, "Overall")
-    st.sidebar.selectbox("Show analysis wrt:", user_list)
+    selected_user = st.sidebar.selectbox("Show analysis wrt:", user_list)
 
     if st.sidebar.button("Show Analysis"):
-        pass
+        num_messages, words, medias, links = helper.fetch_stats(selected_user, df)
+        col1, col2, col3, col4 = st.columns(4)  # Get a list of 4 columns
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Total media shared")
+            st.title(medias)
+        with col4:
+            st.header("Total Links shared")
+            st.title(links)
+        #finding the most active users in the group
+        if selected_user=='Overall':
+            st.title('Most active user')
+            x, new_df=helper.most_busy(df)
+            fig, ax = plt.subplots()
+            col1, col2= st.columns(2)
+            with col1:
+
+                ax.bar(x.index, x.values, color='orange')
+
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+from urlextract import URLExtract
+extract=URLExtract()
+
 def fetch_stats(selected_user, df):
     if selected_user == 'Overall':
         # 1. fetching number of messages
@@ -7,13 +10,29 @@ def fetch_stats(selected_user, df):
         for message in df['user_message']:
             words.extend(message.split())
         medias = df[df['user_message'] == '<Media omitted>'].shape[0]
-        return num_messages, len(words), medias
+    # fetch number of links
+        links=[]
+        for message in df['user_message']:
+            links.extend(extract.find_urls(message))
+        return num_messages, len(words), medias, len(links)
+    #
+
 
     else:
         new_df = df[df['sender'] == selected_user]
         num_messages = new_df.shape[0]
+        links = []
+        for message in new_df['user_message']:
+            links.extend(extract.find_urls(message))
         words = []
         for message in new_df['user_message']:
             words.extend(message.split())
         medias = new_df[new_df['user_message'] == '<Media omitted>'].shape[0]
-        return num_messages, len(words), medias
+        return num_messages, len(words), medias, len(links)
+
+
+def most_busy(df):
+    x=df['sender'].value_counts().head()
+    df=round((df['sender'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'sender': 'name', 'count': 'percent'})
+    return x, df
